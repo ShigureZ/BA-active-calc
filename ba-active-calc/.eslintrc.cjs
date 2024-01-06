188c986f387cb883cec73a540a2b276077e4d474
ShigureZ <shigure.zbs@gmail.com>
2024-01-06 23:48:26 +0800

    init

---
+++ ba-active-calc/.eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:essential',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ ba-active-calc/src/views/active/index.vue
@@ -0,0 +1,148 @@
+<script setup>
+import { ref } from 'vue'
+import * as math from 'mathjs'
+
+const Q9 = [30, 4]
+const Q10 = [25, 4]
+const Q11 = [20, 4]
+const Q12 = [4, 4, 4, 20]
+
+const ap = 15
+
+let itemNeed = ref(['', '', '', ''])
+let itemHave = ref(['', '', '', ''])
+let itemAdd = ref(['', '', '', ''])
+
+let Qtime = ref([])
+
+const num2percent = (num) => {
+  return 1 + num / 100
+}
+
+// 计算数量
+const calc = () => {
+  // 创建材料PT矩阵
+  const QM = math.matrix([
+    [Q9[0] * num2percent(itemAdd.value[0]), 0, 0, Q9[1] * num2percent(itemAdd.value[3])],
+    [0, Q10[0] * num2percent(itemAdd.value[1]), 0, Q10[1] * num2percent(itemAdd.value[3])],
+    [0, 0, Q11[0] * num2percent(itemAdd.value[2]), Q11[1] * num2percent(itemAdd.value[3])],
+    [
+      Q12[0] * num2percent(itemAdd.value[0]),
+      Q12[1] * num2percent(itemAdd.value[1]),
+      Q12[2] * num2percent(itemAdd.value[2]),
+      Q12[3] * num2percent(itemAdd.value[3])
+    ]
+  ])
+
+  // 创建需求矩阵
+  const needM = math.matrix([
+    [itemNeed.value[0] - itemHave.value[0]],
+    [itemNeed.value[1] - itemHave.value[1]],
+    [itemNeed.value[2] - itemHave.value[2]],
+    [itemNeed.value[3] - itemHave.value[3]]
+  ])
+
+  // 计算刷图次数
+  const calc = math.lusolve(QM, needM)
+
+  // 格式化
+  Qtime.value = calc._data.map((e) => {
+    return e < 0 ? 0 : math.ceil(e[0])
+  })
+
+  console.log(Qtime.value)
+}
+
+// 重置输入项
+const reset = () => {
+  itemNeed.value = [0, 0, 0, 0]
+  itemHave.value = [0, 0, 0, 0]
+  itemAdd.value = [0, 0, 0, 0]
+  Qtime.value = []
+}
+
+const setNull = () => {}
+</script>
+
+<template>
+  <div>
+    <h2 style="text-align: center">BA活动刷图计算器 v0.1</h2>
+    <table>
+      <tr>
+        <th></th>
+        <th>浴巾</th>
+        <th>洗发水</th>
+        <th>入浴剂</th>
+        <th>PT</th>
+      </tr>
+      <tr>
+        <td>加成</td>
+        <td v-for="(item, index) in itemAdd" :key="'add' + index">
+          <el-input v-model="itemAdd[index]" type="number" clearable placeholder="请输入加成">
+            <template #append>%</template>
+          </el-input>
+        </td>
+      </tr>
+      <tr>
+        <td>需求</td>
+        <td v-for="(item, index) in itemNeed" :key="'need' + index">
+          <el-input v-model="itemNeed[index]" type="number" clearable placeholder="请输入需求量" />
+        </td>
+      </tr>
+      <tr>
+        <td>持有</td>
+        <td v-for="(item, index) in itemHave" :key="'have' + index">
+          <el-input v-model="itemHave[index]" type="number" clearable placeholder="请输入持有量" />
+        </td>
+      </tr>
+    </table>
+
+    <table v-if="Qtime.length > 0">
+      <tr>
+        <th></th>
+        <th>Q9</th>
+        <th>Q10</th>
+        <th>Q11</th>
+        <th>Q12</th>
+      </tr>
+      <tr>
+        <td>次数</td>
+        <td v-for="(item, index) in Qtime" :key="'time' + index">
+          <el-tag type="success">{{ item }}次</el-tag>
+        </td>
+      </tr>
+      <tr>
+        <td>体力</td>
+        <td v-for="(item, index) in Qtime" :key="'ap' + index">
+          <el-tag type="success">{{ item * ap }}AP</el-tag>
+        </td>
+      </tr>
+    </table>
+    <div class="btn">
+      <el-button type="primary" @click="calc">计算</el-button>
+      <el-button type="danger" @click="reset">重置</el-button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+table {
+  width: 80%;
+  text-align: center;
+  margin: auto;
+  td {
+    padding: 10px 5px;
+    .el-input {
+      width: 250px;
+    }
+  }
+  th {
+    font-size: 20px;
+  }
+}
+
+.btn {
+  @extend table;
+  text-align: right;
+}
+</style>
